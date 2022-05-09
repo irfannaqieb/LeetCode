4cb3b17fbadea8cd1bb26083caeb680643bae6f1
Irfan Naqieb <irfannaqieb@gmail.com>
2022-05-10 01:49:52 +0900

    added Palindrome Number problem solution.

---
+++ reversePolishNotation.py
@@ -0,0 +1,38 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+
+        def isInt(s):
+            try:
+                int(s)
+                return True
+            except:
+                return False
+
+        stack = []
+
+        for i in tokens:
+            if isInt(i):
+                stack.append(i)
+
+            else:
+                b = int(stack.pop())
+                a = int(stack.pop())
+
+                if i == "+":
+                    c = a + b
+                elif i == "-":
+                    c = a - b
+                elif i == "*":
+                    c = a * b
+                elif i == "/":
+                    c = int(a / b)
+
+                stack.append(str(c))
+
+        return stack[-1]
+
+
+
+
+
+
