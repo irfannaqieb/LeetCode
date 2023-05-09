63078aaa953e4a3e6bda9e2a156d00085acc144e
Irfan Baharuddin <irfannaqieb@gmail.com>
2023-05-09 20:10:59 +0900

    added solution for 125

---
+++ 125.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        characters = "0123456789abcdefghijklmnopqrstuvwxyz"
+        arr = []
+
+        lowercase_converted = s.lower()
+
+        for c in lowercase_converted:
+            if c in characters:
+                arr.append(c)
+
+        
+        joined_characters = "".join(arr)
+
+        return joined_characters == joined_characters[::-1]
+
+
+
+
+
